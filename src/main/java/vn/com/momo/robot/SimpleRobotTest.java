5cb9b9fae5f8a715fcca05248580b0ccc5e46bef
titioak <dohuutrong.qng@gmail.com>
2021-12-29 10:51:23 +0700

    second commit

--- src/main/java/vn/com/momo/robot/SimpleRobotTest.java
+++ src/main/java/vn/com/momo/robot/SimpleRobotTest.java
@@ -1,7 +1,9 @@
 package vn.com.momo.robot;
 
-public class SimpleRobotTest {
+import org.junit.jupiter.api.Test;
 
+public class SimpleRobotTest {
+    @Test
     public static void main(String[] args) {
         FullRobot robot = DaggerRobotCombonent.create().buildRobot();
         robot.doIt();
